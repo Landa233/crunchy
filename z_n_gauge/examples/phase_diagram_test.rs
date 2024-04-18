cbbbb2ce330c50ffdce4ad35c882dc5ee06f636f
Landa233 <david_lanners@hotmail.fr>
2024-04-18 20:50:40 +0100

    added sbash

--- z_n_gauge/examples/phase_diagram_test.rs
+++ z_n_gauge/examples/phase_diagram_test.rs
@@ -1,6 +1,4 @@
 #![feature(generic_const_exprs)]
-#![allow(incomplete_features)]
-
 use z_n_gauge::sheduler::{
     executor::HaltingCondition,
     phase_diagram::phase_diagram_sweep::{phase_diagram_sweep, PhaseDiagram, Range},
@@ -10,14 +8,14 @@ fn main() {
     const ZORDER: usize = 3;
 
     let phase_diagram = PhaseDiagram::<ZORDER> {
-        beta_range: Range::new([0.49, 0.56], 24),
+        beta_range: Range::new([0.01, 0.7], 128),
         lambda_range: Range::new([1.0, 1.0], 1),
         experiments_per_point: 1,
-        number_of_threads: 8,
-        halting_condition: HaltingCondition::Recordings(1000),
+        number_of_threads: 128,
+        halting_condition: HaltingCondition::Recordings(20000),
         recording_skip: 1,
-        recordings_until_backup: 1234,
-        lattice_shape: [4, 4, 4, 4],
+        recordings_until_backup: 3000,
+        lattice_shape: [12, 12, 12, 12],
         folder_name: "../../../nobackup/jhtb65/_recordings".to_string(),
     };
 
