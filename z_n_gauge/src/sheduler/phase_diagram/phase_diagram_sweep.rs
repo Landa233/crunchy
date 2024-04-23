d32b619a555a702e5200e94ef41f1408a9487003
Landa233 <david_lanners@hotmail.fr>
2024-04-23 08:18:18 +0100

    fixed tmp storage sbatch

--- z_n_gauge/src/sheduler/phase_diagram/phase_diagram_sweep.rs
+++ z_n_gauge/src/sheduler/phase_diagram/phase_diagram_sweep.rs
@@ -55,7 +55,7 @@ impl<const ZORDER: usize> PhaseDiagram<ZORDER> {
             #SBATCH -c {}\n\
             #SBATCH --time={}-{}:{}:{}\n\
             #SBATCH --mem={}G\n\
-            #SBATCH --gres=tmp:{}\n\
+            #SBATCH --gres=tmp:{}G\n\
             #SBATCH --mail-user={}\n\
             #SBATCH --mail-type=ALL\n\
             #SBATCH -p {}\n\n\n\
